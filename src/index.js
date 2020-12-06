9d4f70381638bf94c2d66d41d4c7a4adc5bd3424
Alecto <fomenkoandrey1978@gmail.com>
2020-12-06 13:58:50 +0200

    первый урок

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-const post = new Post('Webpack Post Title');
+import * as $ from 'jquery';
+import '@css/style.css';
+import Post from '@model/posts';
+import json from '@assets/data.json';
+import webpackLogo from '@assets/icon-square-big.png';
+import xml from '@assets/data.xml';
+import csv from '@/assets/data.csv';
 
-console.log('Post to sting:', post.toString());
+const post = new Post('Webpack Post Title', webpackLogo);
+
+$('pre').html(post.toString())
+
+console.log('JSON:', json);
+console.log('XML:', xml);
+console.log('CSV:', csv);
